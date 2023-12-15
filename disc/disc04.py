2194fae3b93989fba33829cbfa3555d8c0c16ea4
9963KK <1061893965@qq.com>
2023-12-15 21:45:47 +0800

    disc04 done

--- disc/disc04.py
+++ disc/disc04.py
@@ -37,13 +37,38 @@ def count_k(n, k):
     """
     #1. 可以理解为上题目中不再限制每次跨出的步数。
     #2. 理解为递归树上有多少个节点
-    #3. 此题和count_partitions类似
-    if n==1 or k==1 or n==0: return 1
-    elif k==0: return 0
-    with_k=count_k(n-k,k)
-    without_k=count_k(n,k-1)
-    return with_k+without_k
+    #3. 此题和count_partitions类似,但是这个是有顺序的比如说1+2和2+1是不一样的，但在count_partitions中，二者相同。
+    #4. 对于每个数来说，都可以依次从1到k不同的数字组合来组成n，都是不一样的组合。
+    #5. 公式可以写为count_k(n-1,k)+count_k(n-2,k)+count_k
+    """
+    We use a loop in our solution for count_k. The first iteration of the loop counts the ways to go up n steps if we start by taking one step, the second iteration counts the ways to go up n steps if we start by taking two steps, and so on. The very last iteration counts the ways to go up n steps if we start by taking k steps, which is the most we can take at once.
 
+We use the total variable to track the sum of the results of our recursive count_k calls. When the loop ends, total will store the number of ways to go up n stairs and will include every possible starting move.
+
+You can use recursion visualizer to step through the call structure of count_k(3, 3).
+    """
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    else:
+        total = 0
+        for step in range(1, k + 1):
+            total += count_k(n - step, k)
+        return total
+    # ALTERNATE SOLUTION
+def count_k(n, k):
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    else:
+        total = 0
+        i = 1
+        while i <= k:
+            total += count_k(n - i, k)
+            i += 1
+        return total
 
 def paths(m, n):
     """Return the number of paths from one corner of an
@@ -58,7 +83,28 @@ def paths(m, n):
     >>> paths(1, 157)
     1
     """
-    "*** YOUR CODE HERE ***"
+    if m == 1 or n == 1:
+        return 1
+    return paths(m - 1, n) + paths(m, n - 1)
+    # Base case: Look at the two visual examples given. Since the insect
+    # can only move to the right or up, once it hits either the rightmost edge
+    # or the upper edge, it has a single remaining path -- the insect has
+    # no choice but to go straight up or straight right (respectively) at that point.
+    # There is no way for it to backtrack by going left or down.
+    # Alternative solution:
+    if m == 1 and n == 1:
+        return 1
+    if m < 1 or n < 1:
+        return 0
+    return paths(m - 1, n) + paths(m, n - 1)
+    # This solution is similar to the alternate solution for Count Stair Ways.
+    # If we reach the exact destination, we have found a unique path (first base case), but if
+    # we overshoot, we have not found a valid path (second base case).
+
+    # Notice, however, that this solution is not as short and simple as the first solution
+    # since it doesn't make use of the insect's restricted movements (only right or up)
+    # to cut the program short. We have to reach the exact destination for the second solution,
+    # while in the first we just have to reach the right or top boundary.
 
 
 def max_product(s):
@@ -71,7 +117,41 @@ def max_product(s):
     >>> max_product([])
     1
     """
+    if s == []:
+        return 1
+    if len(s) == 1:
+        return s[0]
+    else:
+        return max(s[0] * max_product(s[2:]), max_product(s[1:]))
+        # OR
+        return max(s[0] * max_product(s[2:]), s[1] * max_product(s[3:]))
+    """
+This solution begins with the idea that we either include s[0] in the product or not:
 
+If we include s[0], we cannot include s[1].
+If we don't include s[0], we can include s[1].
+The recursive case is that we choose the larger of:
+
+multiplying s[0] by the max_product of s[2:] (skipping s[1]) OR
+just the max_product of s[1:] (skipping s[0])
+Here are some key ideas in translating this into code:
+
+The built-in max function can find the larger of two numbers, which in this case come from two recursive calls.
+In every case, max_product is called on a list of numbers and its return value is treated as a number.
+An expression for this recursive case is:
+
+max(s[0] * max_product(s[2:]), max_product(s[1:]))
+
+Since this expression never refers to s[1], and s[2:] evaluates to the empty list even for a one-element list s, the second base case (len(s) == 1) can be omitted if this recursive case is used.
+
+The recursive solution above explores some options that we know in advance will not be the maximum, such as skipping both s[0] and s[1]. Alternatively, the recursive case could be that we choose the larger of:
+
+multiplying s[0] by the max_product of s[2:] (skipping s[1]) OR
+multiplying s[1] by the max_product of s[3:] (skipping s[0] and s[2])
+An expression for this recursive case is:
+
+max(s[0] * max_product(s[2:]), s[1] * max_product(s[3:]))
+    """
 
 def flatten(s):
     """Returns a flattened version of list s.
@@ -87,6 +167,12 @@ def flatten(s):
     >>> flatten(very_deep)
     ['m', 'i', 'n', 'm', 'e', 'w', 't', 'a', 't', 'i', 'o', 'n', 's']
     """
-    "*** YOUR CODE HERE ***"
+    lst = []
+    for elem in s:
+        if type(elem) == list:
+            lst += flatten(elem)
+        else:
+            lst += [elem]
+    return lst
 #Test
 count_k(3,3)
